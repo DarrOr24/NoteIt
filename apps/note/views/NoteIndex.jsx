3be6627b94f42f6a15c40ff6d2e1c3a5862dcdb8
Darr Or <darrmorgan@gmail.com>
2024-10-18 14:37:12 -0400

    Adjust header and cleanup

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -12,43 +12,41 @@ export function NoteIndex() {
 
     const [notes, setNotes] = useState([])
     const [isLoading, setIsLoading] = useState(false)
-    const [searchParams, setSearchParams] = useSearchParams()
-    // const [ filterBy, setFilterBy ] = useState(noteService.getFilterFromSearchParams(searchParams))
-    const [ filterBy, setFilterBy ] = useState({})
-    const [ showFilterandAdd, setShowFilterAndAdd ] = useState(true) 
-    
-    
+    const [filterBy, setFilterBy] = useState({})
+    const [showFilterandAdd, setShowFilterAndAdd] = useState(true)
+
+
     useEffect(() => {
         noteService.query(filterBy)
             .then(notes => setNotes(notes))
     }, [filterBy])
 
-    function mainDisplay(stat){
+    function mainDisplay(stat) {
         setShowFilterAndAdd(stat)
 
     }
 
-    function addNewNote(note){//Note already saved to service
+    function addNewNote(note) {//Note already saved to service
         if (!note.isPinned) setNotes([...notes, note])
         else setNotes([note, ...notes])
     }
 
-    function onEdit(noteToEdit){ //Note already saved to service
+    function onEdit(noteToEdit) { //Note already saved to service
         placeNote(noteToEdit)
         noteService.save(noteToEdit)
-        .then (placeNote)
+            .then(placeNote)
 
     }
 
-    function onRemove(ev, noteToTrash){
+    function onRemove(ev, noteToTrash) {
         ev.stopPropagation()
         const noteId = noteToTrash.id
         console.log('oh yeah!!  triple callback!!')
         setIsLoading(true)
         setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId))
-        
+
         noteService.saveToTrash(noteToTrash)
-        
+
         noteService.remove(noteId)
             .then(() => {
                 setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId))
@@ -59,25 +57,25 @@ export function NoteIndex() {
             .finally(() => setIsLoading(false))
     }
 
-    function onPinNote(noteFromPin){
-        noteService.save({...noteFromPin, isPinned: noteFromPin.isPinned, pinTime: (noteFromPin.isPinned) ? Date.now() : ''})
-        .then(placeNote)
+    function onPinNote(noteFromPin) {
+        noteService.save({ ...noteFromPin, isPinned: noteFromPin.isPinned, pinTime: (noteFromPin.isPinned) ? Date.now() : '' })
+            .then(placeNote)
     }
 
-    function placeNote(noteToPlace){
+    function placeNote(noteToPlace) {
         console.log('note reached index', noteToPlace.isPinned)
         //Note already saved in the service
         //First take her out and set her again with all the notes
         const restOfNotes = notes.filter(note => note.id !== noteToPlace.id)
-        
+
         const unsortedNotes = [...restOfNotes, noteToPlace]
-        
+
         const sortedNotes = noteService.sortNotes(unsortedNotes)
 
         setNotes(sortedNotes)
     }
 
-    function onDuplicate(noteToDuplicate){
+    function onDuplicate(noteToDuplicate) {
         const newNote = structuredClone(noteToDuplicate)
         newNote.id = ''
         newNote.time = Date.now()
@@ -88,19 +86,19 @@ export function NoteIndex() {
     }
 
     function onSetFilterBy(newFilter) {
-        
+
         setFilterBy({ ...newFilter })
     }
 
 
     if (isLoading) return <div className="loader"></div>
-    return <section className = "note-index not-trash full">
+    return <section className="note-index not-trash full">
 
         <header className="note-index-header">
 
             <img height="50" src="assets\img\keep-icon.png" alt="" />
-            <h1>Keep</h1>
-            {showFilterandAdd &&  <NoteFilter filterBy={filterBy} onFilter={onSetFilterBy} />}
+            <h1>Noted</h1>
+            {showFilterandAdd && <NoteFilter filterBy={filterBy} onFilter={onSetFilterBy} />}
 
         </header>
 
@@ -108,17 +106,17 @@ export function NoteIndex() {
 
             <NoteSideMenu mainDisplay={mainDisplay} />
 
-            {showFilterandAdd && <AddNote notes={notes} onAdd={addNewNote} onPinNote ={placeNote} />} 
-                  
+            {showFilterandAdd && <AddNote notes={notes} onAdd={addNewNote} onPinNote={placeNote} />}
+
             <Outlet context={{
-                    notes,
-                    onRemove,
-                    onEdit,
-                    onPinNote,
-                    onDuplicate,
-                }} />
+                notes,
+                onRemove,
+                onEdit,
+                onPinNote,
+                onDuplicate,
+            }} />
         </main>
-        
+
     </section>
 }
 
